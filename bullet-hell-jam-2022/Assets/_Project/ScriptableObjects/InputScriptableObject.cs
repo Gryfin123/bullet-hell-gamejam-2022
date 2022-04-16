ae688eae5c46f8347a0c8e27dc179c2461e7d325
Krystian <28630895+Gryfin123@users.noreply.github.com>
2022-04-16 12:33:36 +0200

    Prepared player gameplay mapping

--- bullet-hell-jam-2022/Assets/_Project/ScriptableObjects/InputScriptableObject.cs
+++ bullet-hell-jam-2022/Assets/_Project/ScriptableObjects/InputScriptableObject.cs
@@ -6,6 +6,9 @@ using UnityEngine.Events;
 
 public class InputScriptableObject : ScriptableObject
 {
+
+    private PlayerInput gameInput;
+
     // Start is called before the first frame update
     void Start()
     {
