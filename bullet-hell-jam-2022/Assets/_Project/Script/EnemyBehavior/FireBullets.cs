011f00cf00bcdd831bb3fa300d1f1274349b7841
Krystian <28630895+Gryfin123@users.noreply.github.com>
2022-04-20 01:18:30 +0200

    Added option to delay, and limit amount of shots enemy can fire

--- bullet-hell-jam-2022/Assets/_Project/Script/EnemyBehavior/FireBullets.cs
+++ bullet-hell-jam-2022/Assets/_Project/Script/EnemyBehavior/FireBullets.cs
@@ -5,18 +5,24 @@ using UnityEngine;
 public class FireBullets : MonoBehaviour
 {
     public bool _canFire = true;
+    public int _fireLimit = 0; // Zero means there is no limit
+    public float _initialFireDelay = 0f;
+    public float _creationTime;
     [SerializeField] private List<BulletFury.BulletManager> _bulletManager; 
+    private int _timesFired = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        _creationTime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (_canFire)
+        if (_canFire 
+            && (_fireLimit == 0 || _fireLimit > _timesFired) 
+            && _creationTime + _initialFireDelay < Time.time)
         {
             foreach(BulletFury.BulletManager curr in _bulletManager)
             {
@@ -25,4 +31,8 @@ public class FireBullets : MonoBehaviour
         }
     }
 
+    public void CountFires()
+    {
+        _timesFired++;
+    }
 }
